3166f60c6a19cc08ae16f89c6a1250fd09e3e5b7
taoruizhe <taoruizhe@inspur.com>
2019-11-26 09:37:09 +0800

    "更新测试"

--- src/utils/progress/bar/ProgressBar.java
+++ src/utils/progress/bar/ProgressBar.java
@@ -1,7 +1,7 @@
 package utils.progress.bar;
 
 /**
- * @head 打印进度条：类似linux的进度条打印
+ * @details 打印进度条：类似linux的进度条打印
  * key: '\b'退格
  * key:  System.out.printf("%+8.3f\n", 3.14) 宽度8，保留3位有效数字
  * @link 1. https://www.cnblogs.com/liuwu265/p/4100092.html 2. https://www.ctolib.com/topics-61227.html
@@ -10,24 +10,22 @@ package utils.progress.bar;
 public class ProgressBar extends Thread{
     private static int elapsed = 0;
 
-    public static void print() {
-        StringBuffer str1 = new StringBuffer("downloading:");
-        StringBuffer str2 = new StringBuffer();
+    public static synchronized void print() {
+        StringBuffer str1 = new StringBuffer("Starting:");
+        StringBuffer str2 = new StringBuffer("\b\b\b\b\b\b\b\b\b\b\b\b\b");
         for (int i = 0; i < 100; i++) {
             str1.append("-");
             str2.append("\b");
         }
-        str2.append("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
         try {
             for(int elapsed = 0; elapsed < 100; elapsed++) {
                 Thread.sleep(elapsed*2);
-                str1.replace(elapsed + 12, elapsed + 14, "=>");
+                str1.replace(elapsed + 9, elapsed + 11, "=>");
                 System.out.print(str2.toString() + str1.toString() );
                 System.out.printf("%3d", elapsed + 1);
                 System.out.print("%");
             }
-            System.out.println("\nWelcome to new WORLD!");
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
