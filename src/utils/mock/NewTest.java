3628814dc6a8c4c861fac21d15a921492af0d76e
taoruizhe <taoruizhe@inspur.com>
2020-04-21 16:37:36 +0800

    leetcode：岛屿的数量

---
+++ src/utils/mock/NewTest.java
@@ -0,0 +1,22 @@
+package utils.mock;
+
+public class NewTest {
+//    public class A {
+//        public String getMessage() {
+//            Cps cps = new Cps;
+//            return cps.getValue();
+//        }
+//    }
+//
+//    @RunWith(PowerMockitoRunner.class)
+//    @PrepareForTest(Cps.class, A.class)
+//    public class ATest {
+//        @Test
+//        public void getMessageTest() {
+//            Cps cps = PowerMockito.mock(Cps.class);
+//            PowerMockito.whenNew(Cps.class).withNoArguments().thenReturn(cps);
+//            when(cps.getValue()).thenReturn("test");
+//        }
+//    }
+
+}
