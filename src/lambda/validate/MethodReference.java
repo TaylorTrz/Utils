4dae3f5b796f3e8cc3bdc30f58e2693edfea44c7
taoruizhe <taoruizhe@inspur.com>
2020-05-26 19:34:56 +0800

    函数式编程练习

--- src/lambda/validate/MethodReference.java
+++ src/lambda/validate/MethodReference.java
@@ -27,11 +27,12 @@ public class MethodReference {
         oi = InnerClass::new;
         System.out.println("构造方法Lambda方法引用: " + oi.getClass().getName());
 
+        TransX transX = (x) -> new X().f();
+        transX = X::f;
+        System.out.println(transX.transform(new X()));
     }
-
-
-
 }
+
 class InnerClass {
     private OuterInterface outerInterface;
     // 静态方法
@@ -54,4 +55,16 @@ class InnerClass {
 
 interface OuterInterface {
     void accept();
+}
+
+// 一种特殊情况：未绑定的方法引用，直接将Object(X)作为引用参数
+// 使用未绑定的引用时，也就是非静态方法的引用。函数方法的签名（接口中的单个方法）可以不与方法引用的签名完全匹配。 只要引用一个对象即可
+class X {
+    String f() {
+        return "X::f";
+    }
+}
+
+interface TransX {
+    String transform(X x);
 }
