3d0d39684de7032ab22ca7f8a86d35719064cbdb
taoruizhe <taoruizhe@inspur.com>
2020-06-28 09:05:10 +0800

    Files与Paths类的操作

--- src/utils/windows/Explorer.java
+++ src/utils/windows/Explorer.java
@@ -3,7 +3,13 @@ package utils.windows;
 import com.sun.istack.NotNull;
 
 import java.io.*;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @description 工具类：文件浏览器
@@ -130,8 +136,49 @@ public class Explorer {
     }
 
 
-    public static void main(String[] args) {
-//        getChildFiles(new File(filePath), "");
-        recurseLookUp(filePath);
+    public static void main(String[] args) throws IOException{
+//        recurseLookUp(filePath);
+
+        // 当前使用的系统
+        System.out.println(System.getProperty("os.name"));
+        // java.nio.Paths与Path的使用
+        Path path = Paths.get("tmp", "new.txt");
+        // Path与URI转换
+        URI uri = path.toUri();
+        // 路径转换为文件，按理来说路径就是有意义的字符串，而不是文件的概念
+        File file = path.toFile();
+        System.out.println(path.toAbsolutePath().getRoot());
+        System.out.println(uri);
+        System.out.println(file.getAbsolutePath());
+
+        // 两种方式都可，对Path类中的每一个path进行拆分，注意是不包括根目录的
+        Path absPath = path.toAbsolutePath();
+        for (Path p : absPath) {
+            System.out.println(p.toAbsolutePath());
+        }
+        for (int i = 0; i < absPath.getNameCount(); i++) {
+            System.out.println(absPath.getName(i).toAbsolutePath());
+        }
+        // 以文件后缀名进行判断Path，结果是失败的
+        System.out.println("文件后缀判断结果：" + absPath.endsWith(".txt"));
+        // 以根目录来归类Path，结果正常
+        System.out.println("根目录判断结果：" + absPath.startsWith(absPath.getRoot()));
+
+        // java.nio.file.Files类
+        // 写入1000行数组
+        UUID uuid = UUID.randomUUID();
+        Random random = new Random(100);
+        byte[] bytes = new byte[1000];
+        random.nextBytes(bytes);
+        Files.write(path, bytes);
+        System.out.println("当前文件行数：" + Files.size(path));
+        // 对字节数据进行读取
+        Files.readAllLines(path).stream().skip(998).map(i -> i).forEach(System.out::print);
+        System.out.println("当前文件行数：" + Files.size(path));
+        // 直接对“小文件”进行读写
+        List<String> lines = Files.readAllLines(path);
+        lines.stream().filter(line -> line.startsWith("E")).map(line -> line + "直接读写").forEach(System.out::println);
+
+
     }
 }
